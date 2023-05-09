3bf7868cfb968a5bab13dabc5b12b4a5f2acec16
Sahil Rzayev <sahilrzayev200d@gmail.com>
2023-05-10 00:47:06 +0300

    added Concrete to BusinessLayer

---
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountProcessManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+
+namespace EasyCashIdentityProject.BusinessLayer.Concrete
+{
+    public class CustomerAccountProcessManager : ICustomerAccountProcessService
+    {
+        private readonly ICustomerAccountProcessDal _customerAccountProcessDal;
+
+        public CustomerAccountProcessManager(ICustomerAccountProcessDal customerAccountProcessDal)
+        {
+            _customerAccountProcessDal = customerAccountProcessDal;
+        }
+
+        public void TInsert(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Insert(t);
+        }
+
+        public void TDelete(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Delete(t);
+        }
+
+        public void TUpdate(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Update(t);
+        }
+
+        public CustomerAccountProcess TGetByID(int id)
+        {
+            return _customerAccountProcessDal.GetByID(id);
+        }
+
+        public List<CustomerAccountProcess> TGetList()
+        {
+            return _customerAccountProcessDal.GetList();
+        }
+    }
+}
