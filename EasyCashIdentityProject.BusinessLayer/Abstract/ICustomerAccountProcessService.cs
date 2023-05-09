8e0bd61160f2c12d6bbb163d9280be4bb9e4d7e7
Sahil Rzayev <sahilrzayev200d@gmail.com>
2023-05-10 00:37:32 +0300

    added Abstract(Interfcaes) to BusinessLayer

---
+++ EasyCashIdentityProject.BusinessLayer/Abstract/ICustomerAccountProcessService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+
+namespace EasyCashIdentityProject.BusinessLayer.Abstract
+{
+    public interface ICustomerAccountProcessService : IGenericService<CustomerAccountProcess>
+    {
+    }
+}
