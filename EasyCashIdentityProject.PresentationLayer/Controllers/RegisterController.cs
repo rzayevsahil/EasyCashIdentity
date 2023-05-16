61eb33ddb78a27a3adac6e7da85031653f00084a
Sahil Rzayev <sahilrzayev200d@gmail.com>
2023-05-16 23:44:49 +0300

    added ConfirmCode and Mail Message

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -1,54 +1,78 @@
 ﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
 using EasyCashIdentityProject.EntityLayer.Concrete;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
-	public class RegisterController : Controller
-	{
-		private readonly UserManager<AppUser> _userManager;
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
 
-		public RegisterController(UserManager<AppUser> userManager)
-		{
-			_userManager = userManager;
-		}
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
 
-		[HttpGet]
-		public IActionResult Index()
-		{
-			return View();
-		}
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
-		{
-			if (ModelState.IsValid)//Fluent Validation'dan geçiyorsa
-			{
-				AppUser appUser = new AppUser()
-				{
-					UserName = appUserRegisterDto.Username,
-					Name = appUserRegisterDto.Name,
-					Surname = appUserRegisterDto.Surname,
-					Email = appUserRegisterDto.Email,
-					City = "aaaa",
-					District = "bbbb",
-					ImageUrl = "cccc"
-				};
-				var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
-				if (result.Succeeded)
-				{
-					return RedirectToAction("Index", "ConfirmMail");
-				}
-				else
-				{
-					foreach (var item in result.Errors)
-					{
-						ModelState.AddModelError("",item.Description);
-					}
-				}
-			}
-			return View();
-		}
-	}
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
+        {
+            if (ModelState.IsValid)//Fluent Validation'dan geçiyorsa
+            {
+                Random random = new Random();
+                int code = random.Next(100000, 1000000);
+                AppUser appUser = new AppUser()
+                {
+                    UserName = appUserRegisterDto.Username,
+                    Name = appUserRegisterDto.Name,
+                    Surname = appUserRegisterDto.Surname,
+                    Email = appUserRegisterDto.Email,
+                    City = "aaaa",
+                    District = "bbbb",
+                    ImageUrl = "cccc",
+                    ConfirmCode = code
+                };
+                var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
+                if (result.Succeeded)
+                {
+                    MimeMessage mimeMessage = new MimeMessage();
+                    MailboxAddress mailboxAddressFrom = new MailboxAddress("Easy Cash Admin", "sahilrzayev200d@gmail.com");
+                    MailboxAddress mailboxAddressTo = new MailboxAddress("User", appUser.Email);
+
+                    mimeMessage.From.Add(mailboxAddressFrom);
+                    mimeMessage.To.Add(mailboxAddressTo);
+
+                    var bodyBuilder = new BodyBuilder();
+                    bodyBuilder.TextBody = "Kayıt işlemi gerçekleştirmek için onay kodunuz:" + code;
+                    mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+                    mimeMessage.Subject = "Easy Cash Onay Kodu";
+
+                    SmtpClient smtpClient = new SmtpClient();
+                    smtpClient.Connect("smtp.gmail.com", 587, false);
+                    smtpClient.Authenticate("sahilrzayev200d@gmail.com", "mvvcyvvnrsozkrur");
+                    smtpClient.Send(mimeMessage);
+                    smtpClient.Disconnect(true);
+
+                    return RedirectToAction("Index", "ConfirmMail");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View();
+        }
+    }
 }
