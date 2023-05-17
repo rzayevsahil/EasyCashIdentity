673335c40caac3c753aefee857cef8d9c4b630a8
Sahil Rzayev <sahilrzayev200d@gmail.com>
2023-05-18 02:38:22 +0300

    added ConfirmMailController and ConfirmMailViewModel

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -62,6 +62,8 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
                     smtpClient.Send(mimeMessage);
                     smtpClient.Disconnect(true);
 
+                    TempData["Mail"] = appUserRegisterDto.Email;
+
                     return RedirectToAction("Index", "ConfirmMail");
                 }
                 else
