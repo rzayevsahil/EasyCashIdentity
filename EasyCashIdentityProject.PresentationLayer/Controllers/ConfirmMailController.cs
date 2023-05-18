6de8c6279fb4c5259a8eb866a3f4942ec8478c70
Sahil Rzayev <sahilrzayev200d@gmail.com>
2023-05-18 20:55:11 +0300

    Updated ConfirmMailViewModel and ConfirmMailController,Index pages. Added MyProfileController and Index.cshtml

--- EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
@@ -1,21 +1,34 @@
-﻿using EasyCashIdentityProject.PresentationLayer.Models;
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using EasyCashIdentityProject.PresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
     public class ConfirmMailController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        public ConfirmMailController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [HttpGet]
-        public IActionResult Index(int id)
+        public IActionResult Index()
         {
             var value = TempData["Mail"];
-            ViewBag.v = value + "aaa";
+            ViewBag.v = value;
             return View();
         }
 
         [HttpPost]
-        public IActionResult Index(ConfirmMailViewModel confirmMailViewModel)
+        public async Task<IActionResult> Index(ConfirmMailViewModel confirmMailViewModel)
         {
+            var user = await _userManager.FindByEmailAsync(confirmMailViewModel.Mail);
+            if (user.ConfirmCode == confirmMailViewModel.ConfirmCode)
+            {
+                return RedirectToAction("Index", "MyProfile");
+            }
             return View();
         }
     }
